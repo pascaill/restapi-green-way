0d099958da62ce0327f39077f29d7d8c98e5abb6
Ria <songriah98@gmail.com>
2024-05-18 11:11:36 +0700

    edited app.js

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
+// app.js
 import express from 'express';
 import bodyParser from 'body-parser';
-import wisataRoutes from './routes/wisata.js';
-import champaignRoutes from './routes/champaign.js';
-import communityRoutes from './routes/community.js';
 import authRoutes from './routes/auth.js';
+import wisataRoutes from './routes/wisata.js';
+import communityRoutes from './routes/community.js';
+import champaignRoutes from './routes/champaign.js';
+import commentRoutes from './routes/comment.js';
 
-// Inisialisasi aplikasi Express
 const app = express();
 app.use(bodyParser.json());
 
-// Tambahkan logging middleware
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
-
-// Gunakan rute dari folder routes
-app.use('/wisata', wisataRoutes);
-app.use('/champaign', champaignRoutes);
-app.use('/community', communityRoutes);
 app.use('/auth', authRoutes);
+app.use('/wisata', wisataRoutes);
+app.use('/community', communityRoutes);
+app.use('/champaign', champaignRoutes);
+app.use('/comment', commentRoutes);
 
-// Port aplikasi
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
